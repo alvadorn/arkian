85dfa3cf10cb99b3710bef9c6a37eeaea609138b
Igor Sant'ana <igorba2005@gmail.com>
2015-03-21 23:03:00 -0300

    Encoder is now working with one and two arguments.

--- spec/text_processing/encoder_spec.rb
+++ spec/text_processing/encoder_spec.rb
@@ -1,14 +1,43 @@
 require_relative '../../src/text_processing/encoder'
 
-describe 'Encoder' do
-	context 'memory arg should return a hash' do
-		it 'from bx+si+9' do
-			string = '[bx+si+9]'
-			expected = { memory: { exp: :bx_si, imed: 9 } }
-			result = Encoder.memory_access(string)
+
+describe Encoder do
+	class EncoderClass
+		include Encoder
+	end
+	
+	before(:all) do
+		@encoder = EncoderClass.new
+	end
+
+	context 'memory with one argument' do
+		it '[SI]' do
+			string = '[SI]'
+			expected = { memory: { exp: ["SI"] } }
+			result = @encoder.memory_access(string)
 
 			expect(result).to eq(expected)
 		end
+
+		it 'imediate [1000]' do
+			string = '[1000]'
+			result = @encoder.memory_access(string)
+			expected = { memory: {imed: 1000} }
+			expect(result).to eq(expected)
+		end
+	end
+
+	context 'memory with two arguments' do
+		it '[SI+9]' do
+			result = @encoder.memory_access('[SI+9]')
+			shoulda = { memory: { exp: ["SI"], imed: 9} }
+			expect(result).to eq(shoulda)
+
+		end
+		it '[BP+SI]' do
+			result = @encoder.memory_access('[BP+SI]')
+			shoulda = { memory: { exp: %w(BP SI) } }
+			expect(result).to eq(shoulda)
+		end
 	end
 end
-
--- src/text_processing/encoder.rb
+++ src/text_processing/encoder.rb
@@ -1,11 +1,46 @@
 module Encoder
+	
 	# Object to be returned:
-	# memory: { exp: exp [, imed: imed]}
+	# { memory: { exp: exp [, imed: imed] } }
 	def memory_access(string)
-		string.gsub!(/(\s|\[|\])/, '')
-		obj = string.match(//)[0].gsub(/(\+|\-)/, '_#{string.match(/(\+|\-)/)[0]}_')
-		obj
+		# string.gsub!(/(\s|\[|\])/, '')
+		values = string.upcase.scan(/([B,S][P,I,X])?(\-?\d*)/).flatten.delete_if { |v| v == "" || v == nil }
+
+		raise "ERROR: Bad format in line" if values.empty?
+
+		result = {}
+		
+		case values.length
+		when 1
+			result[:memory] = one_arg(values)
+		when 2 
+			result[:memory] = two_args(values)
+		end
+		result
+	end
+	
+	private 
+
+	def is_number?(args)
+		!args[0].scan(/\d+/).empty?
 	end
 
-	module_function :memory_access
+	def two_args(args)
+		if is_number? args[1]
+			{ exp: [args[0]], imed: args[1].to_i }
+		else
+			{ exp: args }
+		end
+	end
+
+	def one_arg(args)
+		if is_number? args
+			{ imed: args[0].to_i }
+		else
+			{ exp: args }
+		end
+	end
+
+
+#	module_function :one_arg, :is_number?
 end
