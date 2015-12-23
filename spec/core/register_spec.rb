52c5595989b9b0d48a8597c363eff3cde17366c3
Igor Sant'ana <igorba2005@gmail.com>
2015-12-23 14:59:40 -0200

    Refactored register class

--- spec/core/register_spec.rb
+++ spec/core/register_spec.rb
@@ -2,9 +2,9 @@ require_relative '../../src/core/register'
 
 require 'minitest/autorun'
 
-describe Core::Register do
+describe Emulator::Core::Register do
   before do
-    @register = Core::Register.new
+    @register = Emulator::Core::Register.new
   end
 
   describe 'allocating new register' do
@@ -15,46 +15,46 @@ describe Core::Register do
     end
 
     it 'and saving in low' do
-      @register.set(0xFF, :low)
+      @register.low = 0xFF
       @register.low.must_equal 0xFF
       @register.high.must_equal 0x00
       @register.word.must_equal 0xFF
     end
 
     it 'and saving in high' do
-      @register.set(0xFF, :high)
+      @register.high = 0xFF
       @register.high.must_equal 0xFF
       @register.low.must_equal 0x00
       @register.word.must_equal 0xFF00
     end
 
     it 'and saving in word' do
-      @register.set(0xF236)
+      @register.word = 0xF236
       @register.word.must_equal 0xF236
       @register.low.must_equal 0x36
       @register.high.must_equal 0xF2
     end
 
     it 'and resaving low value' do
-      @register.set(0xFF22)
-      @register.set(0x44, :low)
+      @register.word = 0xFF22
+      @register.low = 0x44
       @register.word.must_equal 0xFF44
       @register.low.must_equal 0x44
       @register.high.must_equal 0xFF
     end
 
     it 'and resaving high value' do
-      @register.set(0x3322)
-      @register.set(0x99, :high)
+      @register.word = 0x3322
+      @register.high = 0x99
       @register.word.must_equal 0x9922
       @register.high.must_equal 0x99
       @register.low.must_equal 0x22
     end
 
     it 'and resaving all values' do
-      @register.set(0x3424)
-      @register.set(0x36, :low)
-      @register.set(0xF7, :high)
+      @register.word = 0x3424
+      @register.low = 0x36
+      @register.high = 0xF7
       @register.word.must_equal 0xF736
       @register.high.must_equal 0xF7
       @register.low.must_equal 0x36
--- src/core/register.rb
+++ src/core/register.rb
@@ -1,32 +1,35 @@
-module Core
-  class Register
-    def initialize
-      @info = { word: 0, high: 0, low: 0 }
-    end
+module Emulator
+  module Core
+    class Register
+      attr_reader :word, :high, :low
 
-    def set(value, size = :word)
-      @info = case size
-							when :low
-								{ word: (@info[:high] << 8) + value, high: @info[:high],
-									low: value }
-							when :high
-								{ word: (value << 8) + @info[:low], high: value,
-									low: @info[:low] }
-							else
-								{ word: value, high: (value >> 8) & 0xFF, low: value & 0xFF }
-							end
-    end
+      def initialize
+        @word = 0
+        @high = 0
+        @low = 0
+      end
 
-    def low
-      @info[:low]
-    end
 
-    def high
-      @info[:high]
-    end
+      def word=(value)
+        @word = value & 0xFFFF
+        @low = value & 0xFF
+        @high = (value >> 8) & 0xFF
+      end
 
-    def word
-      @info[:word]
+      def high=(value)
+        @high = value & 0xFF
+        word_attrib
+      end
+
+      def low=(value)
+        @low = value & 0xFF
+        word_attrib
+      end
+
+      private
+      def word_attrib
+        @word = (@high << 8) + @low
+      end
     end
   end
 end
