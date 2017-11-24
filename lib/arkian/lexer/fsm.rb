f008e8f9a41d199f3d7d7f7e72722dddf4d3798d
Igor Sant'ana <igorba2005@gmail.com>
2017-11-24 17:58:21 -0200

    Single keyword

--- lib/arkian/lexer/fsm.rb
+++ lib/arkian/lexer/fsm.rb
@@ -1,10 +1,45 @@
 module Arkian
   module Lexer
     class FSM
-      
       def initialize
+        @tree = {}
+        @state = 1
+        @state_counter = 1
       end
 
+      def new_keyword(keyword)
+        process_keyword keyword
+      end
+
+      def graph_length
+        @tree.keys.length
+      end
+
+      def graph
+        @tree
+      end
+
+      private
+
+      def process_keyword(keyword)
+        length = keyword.length
+        keyword.chomp.downcase.each_char.with_index(1) do |c, i|
+          @tree[@state] = {} unless @tree.key? @state
+
+          if @tree[@state].key? c
+            @state = @tree[@state][c]
+          else
+            if length == i
+              @tree[@state][c] = 0
+              @state = 1
+            else
+              @state_counter += 1
+              @tree[@state][c] = @state_counter
+              @state = @state_counter
+            end
+          end
+        end
+      end
     end
   end
 end
--- spec/arkian/lexer/fsm_spec.rb
+++ spec/arkian/lexer/fsm_spec.rb
@@ -2,4 +2,55 @@ require 'spec_helper'
 require 'arkian/lexer/fsm'
 
 describe Arkian::Lexer::FSM do
+  describe 'with 1 keyword' do
+    before do
+      @fsm = Arkian::Lexer::FSM.new
+    end
+
+    it 'is 0-length graph with blank keyword' do
+      @fsm.new_keyword ''
+      @fsm.graph_length.must_equal 0
+    end
+
+    it 'is 3-length graph valid when keyword is ADD' do
+      @fsm.new_keyword 'ADD'
+
+      result = {
+        1 => {
+          'a' => 2
+        },
+        2 => {
+          'd' => 3
+        },
+        3 => {
+          'd' => 0
+        }
+      }
+
+      @fsm.graph.must_equal result
+      @fsm.graph_length.must_equal 3
+    end
+
+    it 'is 4-length graph valid when keyword is IDIV' do
+      @fsm.new_keyword 'IDIV'
+
+      result = {
+        1 => {
+          'i' => 2
+        },
+        2 => {
+          'd' => 3
+        },
+        3 => {
+          'i' => 4
+        },
+        4 => {
+          'v' => 0
+        }
+      }
+
+      @fsm.graph.must_equal result
+      @fsm.graph_length.must_equal 4
+    end
+  end
 end
